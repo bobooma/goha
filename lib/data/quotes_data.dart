c28c3b63b6d63484c49dc023fd95f08386b1a4d2
Ahmed Rabie <amartsma@gmail.com>
2021-10-21 07:37:30 +0200

    test_clone

---
+++ lib/data/quotes_data.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:my_quotes/models/quotes_model.dart';
+
+// ignore: constant_identifier_names
+const QUOTES_PATH = "quotes";
+
+//
+class QuotesData extends ChangeNotifier {
+  late StreamSubscription<Event> quoteStream;
+
+  final _database = FirebaseDatabase.instance.reference();
+  List<QuoteModel> _quotes = [];
+  late final String adgh;
+
+  @override
+  void dispose() {
+    quoteStream.cancel();
+    super.dispose();
+  }
+
+  List<QuoteModel> get quotes {
+    return _quotes;
+  }
+
+  void ordersLisetener() {
+    quoteStream = _database.child(QUOTES_PATH).onValue.listen((event) {
+      final alllQuotes = Map<String, dynamic>.from(event.snapshot.value);
+
+      _quotes = alllQuotes.values
+          .map((quoteAsJson) =>
+              QuoteModel.fromRTDB(Map<String, dynamic>.from(quoteAsJson)))
+          .toList();
+      notifyListeners();
+    });
+  }
+}
